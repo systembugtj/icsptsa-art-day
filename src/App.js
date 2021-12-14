5777f4a424fc94627766a93eaa5f31a2c8f809db
Albert Li <albert.li@salesforce.com>
2021-12-13 16:07:23 -0800

    fix: app layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import Container from "react-bootstrap/Container";
-import NavBar from "./components/NavBar/NavBar";
+
 import { Provider } from "react-redux";
 import { ReduxRouter } from "@lagunovsky/redux-react-router";
 import AppRoutes from "./routes";
@@ -9,39 +8,12 @@ import configureStore, { history } from "./configureStore";
 import PropTypes from "prop-types";
 import { BrowserRouter, Outlet, Route, Routes, Link } from "react-router-dom";
 
-import Home from "./components/Home/Home";
-
-const About = () => <span>About</span>;
-
-const Users = () => <span>Users</span>;
-
-function Layout() {
-    return (
-        <Container classname="p3">
-            {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-            <NavBar />
-
-            {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-            <Outlet />
-        </Container>
-    );
-}
-
 const store = configureStore({});
 function App() {
     return (
         <Provider store={store}>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route path="/home" element={<Home />}></Route>
-                        <Route path="/about" element={<About />}></Route>
-                        <Route path="/users" element={<Users />}></Route>
-                    </Route>
-                </Routes>
+                <AppRoutes />
             </BrowserRouter>
         </Provider>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Layout from "./components/Layout/Layout";
 
 const About = () => <span>About</span>;
 
@@ -9,9 +10,11 @@ const Users = () => <span>Users</span>;
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/home" component={Home}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route path="/users" component={Users}></Route>
+            <Route path="/" element={<Layout />}>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/users" element={<Users />}></Route>
+            </Route>
         </Routes>
     );
 }
